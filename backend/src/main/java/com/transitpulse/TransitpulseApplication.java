78daf775d30edebf31bb4a5c35a9d886c5001b68
Parithosh Poojary <parithosh21129917@gmail.com>
2024-08-31 18:59:18 +0530

    updated docker files and backend framework

---
+++ backend/src/main/java/com/transitpulse/TransitpulseApplication.java
@@ -0,0 +1,13 @@
+package com.transitpulse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransitpulseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransitpulseApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/transitpulse/TransitpulseApplicationTests.java
@@ -0,0 +1,9 @@
+package com.transitpulse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransitpulseApplicationTests {
+
+}
